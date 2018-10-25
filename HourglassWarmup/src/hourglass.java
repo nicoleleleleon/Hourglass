0df611c40c663232c76fedc8218b908109ca8eeb
Nicole <Nicole@FireNinja>
2018-10-24 23:19:54 -0700

    :( v bad

--- HourglassWarmup/src/hourglass.java
+++ HourglassWarmup/src/hourglass.java
@@ -2,42 +2,33 @@
 public class hourglass {
 		public static void main(String[] args) {
 			base();
-			tophalf();
-			//middle();
+			topHalf();
+			middle();
 			//bottomhalf();
 			base();
 		}
+
 	public static void base() {
 		System.out.println("|\"\"\"\"\"\"\"\"\"\"|");
 	}
-	public static void space() {
-		System.out.print(" ");
-	}
-	public static void side() {
-		System.out.print("\\");
-	}
+
 	public static void middle() {
-		System.out.println("||");
+		System.out.println("     ||");
 	}
-	public static void side2() {
-		System.out.print("/");
-	}
-	public static void inside() {
-		System.out.print(";");
-	}
-	public static void ln() {
-		System.out.println();
-	}
+
 	public static void topHalf(){
-		for (int space = 1;space<=4;space++) {
-			space();
-		} side();
-		for (int inside = 8; inside>=2; inside-=2) {
-			inside();
-		} side2();
+		for(int line = 0; line < 4; line++) {
+			System.out.print(" ");
+				System.out.print("\\");
+				for(int colon = 8; colon>=2; colon-=2) {
+					System.out.print(":");
+				}
+						System.out.println("/");
+				
+		}
 	}
-	
 }
+
 	
 	
 		
