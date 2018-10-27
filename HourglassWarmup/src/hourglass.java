f36d0a413d12b58f8915a21ba64064b976e5be9a
Nicole <Nicole@FireNinja>
2018-10-26 23:16:05 -0700

    Version 3 done (no ext)

--- HourglassWarmup/src/hourglass.java
+++ HourglassWarmup/src/hourglass.java
@@ -19,40 +19,38 @@ public class hourglass {
 	public static void topHalf(){
 		
 		for(int line = 1; line < 5; line++) {
-				for(int space = 0; space<line; space++) {
+				for(int space = 0; space<line; space++) {//spaces
 						System.out.print(" ");
 				}
 				System.out.print("\\");
 				
-				for(int colons = 8; colons>0+(line-1)*2; colons--) {
+				for(int colons = 8; colons>0+(line-1)*2; colons--) {//worked out formula for number of colons and line#
 					System.out.print(":");
 
 				}
-						System.out.println("/");
+						System.out.println("/"); //closes it off
 				
 		}
 	}
 	public static void botHalf(){
-		
+		int count = 0;
 		for(int line = 5; line > 1; line--) {
-				for(int space = line; space>0; space--) {
+				for(int space = line; space>1; space--) {//makes spaces
 						System.out.print(" ");
 				}
 				System.out.print("/");
-				int count = 0;
+				
 				for(int colons = 0; colons < 2 + 2*count; colons++) {
 					System.out.print(":");
-//why doesn't it work?? errorcount++;	
-				}	
-				
-						System.out.println("\\");
-						
+				}				
+						System.out.println("\\"); //closes it off
+						count++; //each time, 2 more colons
 }
 	}
 }
 
 	
 	
-		
+		//10 printlns... hmm.. it works?!
 		
 	
